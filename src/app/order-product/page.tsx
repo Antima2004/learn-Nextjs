4b0edd1b1e51148b2812396e926c27f69342443b
Antima2004 <antimamishra277@gmail.com>
2026-01-29 22:07:17 +0530

    day 2

---
+++ src/app/order-product/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function OrderProductPage() {
+    const router = useRouter();
+    const handleClick = () => {
+        alert("Order placed!");
+        router.push("/");
+    }
+    return (
+        <div>
+            <h1>Order Product Page</h1>
+            <p>This is the order product page.</p>
+            <button onClick={handleClick}>Place Order</button>
+        </div>
+    );
+}
--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,8 +1,21 @@
+//Error Handling Example
+function getRandomInt(count:number){
+    return Math.floor(Math.random()*count);
+}
+
+
+
 export default function ProductDetailsLayout({
     children,
 }:{
     children:React.ReactNode;
 }){
+     // error handling example
+    const random = getRandomInt(2);
+    if(random === 1){
+        throw new Error("Error loading products");
+    }
+
     return(
         <>
         {children}
